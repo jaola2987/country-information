0900e257de4726e9fd065f22319409bb492e9a78
jaola2987 <jaola2987@gmail.com>
2023-08-17 23:12:26 +0300

    first commit

---
+++ app/country/[name]/page.tsx
@@ -0,0 +1,99 @@
+import CountryCard from "@/app/components/country-card/CountryCard";
+import { Country } from "@/app/page";
+import Image from "next/image";
+import Link from "next/link";
+import { FC } from "react";
+interface ICountryDetail {
+  params: {
+    name: string;
+  };
+}
+
+async function getCountryByName(name: string): Promise<Country> {
+  const resp = await fetch(
+    `https://restcountries.com/v3.1/name/${name}?fullText=true`
+  );
+  const country = await resp.json();
+  return country[0];
+}
+
+async function getCountryBorderByName(name: string) {
+  const resp = await fetch(`https://restcountries.com/v3.1/all`);
+  const countries: Country[] = await resp.json();
+
+  const country = countries.find(
+    (country: Country) => country.name.common === name
+  );
+  return country?.borders?.map((border) => {
+    const borderCountry = countries.find((country) => country.cca3 === border)!;
+    return {
+      name: borderCountry.name.common,
+      rusName: borderCountry.translations.rus.common,
+      flag: borderCountry.flags.svg,
+      flagAlt: borderCountry.flags.alt,
+    };
+  });
+}
+
+const CountryDetail = async ({ params }: ICountryDetail) => {
+  const country = await getCountryByName(params.name);
+  const formatter = Intl.NumberFormat("rus", { notation: "compact" });
+  const borderCountries = await getCountryBorderByName(decodeURI(params.name));
+
+  return (
+    <section className="flex flex-col container">
+      <h1 className="text-5xl font-bold text-center text-gray-800 mt-16">
+        {country.translations.rus.official}
+      </h1>
+      <Link href="/" className="flex items-center py-2 gap-1">
+        <Image src="/arrow.svg" alt="go back home" width={24} height={24} />
+        Back
+      </Link>
+      <article className=" flex md:flex-row flex-col justify-between min-w-full p-10 bg-white rounded-xl">
+        <section>
+          {country.capital && (
+            <h2 className="text-xl text-gray-800 mt-3">
+              <b>🏙️ Столица:</b> {country.capital}
+            </h2>
+          )}
+          <h2 className="text-xl text-gray-800 mt-3">
+            <b>🗺️ Регион: </b>
+            {country.region} {country.subregion && `- ${country.subregion}`}
+          </h2>
+          <h2 className="text-xl text-gray-800 mt-3">
+            <b>👨‍👩‍👧‍👦 Население: </b> {formatter.format(country.population)}
+          </h2>
+          {country.languages && (
+            <h2 className="text-xl text-gray-800 mt-3">
+              <b>🗣️ Язык:</b>{" "}
+              {Object.values(country.languages).map((language) => (
+                <span
+                  key={language}
+                  className="inline-block px-2 bg-indigo-700 mr-2 text-white text-sm rounded-full"
+                >
+                  {language}
+                </span>
+              ))}
+            </h2>
+          )}
+        </section>
+        <div className="relative h-48 my-2 md:h-auto w-96 shadow-md md:order-last order-first">
+          <Image src={country.flags.svg} alt={country.flags.alt} fill />
+        </div>
+      </article>
+
+      <section>
+        <h3 className="mt-12 text-2xl font-semibold text-gray-800">
+          Neighbour countries
+        </h3>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full container gap-2">
+          {borderCountries?.map((border) => (
+            <CountryCard key={border.name} {...border} />
+          ))}
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default CountryDetail;
---
+++ app/page.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import Link from "next/link";
+import CountryCard from "./components/country-card/CountryCard";
+
+export interface Country {
+  name: {
+    common: string;
+  };
+  flags: {
+    svg: string;
+    alt: string;
+  };
+  translations: {
+    rus: {
+      common: string;
+      official: string;
+    };
+  };
+  capital: string;
+  region: string;
+  subregion: string;
+  population: number;
+  languages: {
+    [key: string]: string;
+  };
+  borders?: string[];
+  cca3: string;
+}
+
+async function getCountries(): Promise<Country[]> {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return response.json();
+}
+
+export default async function Home() {
+  const countries = await getCountries();
+
+  return (
+    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full container gap-2 mt-16">
+      {countries.map((country) => (
+        <CountryCard
+          key={country.name.common}
+          name={country.name.common}
+          rusName={country.translations.rus.common}
+          flag={country.flags.svg}
+          flagAlt={country.flags.alt}
+        />
+      ))}
+    </section>
+  );
+}
